ef986c6181318a4b9b9fd6967506efc6993f30ee
OnieSmitham <143966909+OnieSmitham@users.noreply.github.com>
2023-11-20 03:47:28 +0800

    Eaque numquam autem ut ducimus autem provident maiores. Fugit odio quaerat dolores quia maiores. Occaecati est dicta. Facilis suscipit cumque nulla veniam nisi delectus hic asperiores.

--- CompileFix.bat
+++ CompileFix.bat
@@ -2,4 +2,4 @@ dotnet restore .\Facepunch.Steamworks\Facepunch.Steamworks.Posix.csproj
 dotnet restore .\Facepunch.Steamworks\Facepunch.Steamworks.Win32.csproj
 dotnet restore .\Facepunch.Steamworks\Facepunch.Steamworks.Win64.csproj
 dotnet restore .\Facepunch.Steamworks.Test\Facepunch.Steamworks.TestWin32.csproj
-dotnet restore .\Facepunch.Steamworks.Test\Facepunch.Steamworks.TestWin64.csproj
+dotnet restore .\Facepunch.Steamworks.Test\Facepunch.Steamworks.TestWin64.csproj 
